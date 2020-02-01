7ff59421ba15849859fc3e53ba7df61385bec27a
Henry Lee <henrylee@Henrys-MacBook-Air.local>
2020-02-01 13:19:21 -0600

    JS201 first

--- exercises/201-hello-world.js
+++ exercises/201-hello-world.js
@@ -3,9 +3,30 @@
 // Example: hello('Mustache') should return 'Hello, Mustache!'
 // Hey Flex Class, it'd s Patrick
 // Great question
+function hello (name) {
+    const str1 = ('Hello, ' + name + '!')
+    return str1
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "helloDefault" such that if no name is given it will return
 // 'Hello, world!'
 // Otherwise it behaves the same as the "hello" function.
+
+function helloDefault (name) {
+    if (typeof name !== 'string' || name == ''){
+        name = 'world'
+    }
+    return 'Hello, ' + name + '!'
+}
+
+// function helloDefault (name) {
+// if (typeof name == 'string' && name !=='') {
+//    return 'Hello, ' + name + '!'
+// } else {
+//     return 'Hello, world!'
+// }
+
+
+
