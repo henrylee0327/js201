feeab01864a995b025d3169d06f05e49885cd53f
Henry Lee <henrylee@Henrys-MacBook-Air.local>
2020-02-06 19:12:49 -0600

    JS201 fizzbuzz

--- exercises/210-fizzbuzz.js
+++ exercises/210-fizzbuzz.js
@@ -9,3 +9,116 @@
 // Examples:
 // fizzbuzz(3) --> '..fizz'
 // fizzbuzz(15) --> '..fizz.buzzfizz..fizzbuzz.fizz..fizzbuzz'
+
+function fizzbuzz (inputNumber) {
+    var str1 = ""
+
+    for (var index = 1; index <= inputNumber; i++) {
+        if (index % 3 !== 0 && index % 5 !== 0) {
+            str1 = str1 + "."
+        }
+        else if (index % 3 === 0 && index % 5 !== 0) {
+            str1 = str1 + "fizz"
+        }
+        else if (index % 5 === 0 && index % 3 !== 0) {
+            str1 = str1 + "buzz"
+        }
+        else if (index % 3 === 0 && index % 5 === 0) {
+            str1 = str1 + "fizzbuzz"
+        }
+    }   return str1
+}
+
+
+
+
+// function fizzbuzz (inputNumber) {
+//     var str1 = ""
+    
+//     for (var index = 1; index <= inputNumber; i++){
+//         if (index % 3 !== 0 && index % 5 !== 0) {
+//             str1 = str1.concat(".")
+//         }
+//         else if (index % 3 === 0 && index % 5 !== 0) {
+//             str1.concat("fizz")
+//         }
+//        else if (index % 5 === 0 && index % 3 !== 0) {
+//         str1.concat("buzz")
+//     }   
+//         else if (index % 3 === 0 && index % 5 ===0) {
+//             str1.concat("fizzbuzz")
+//         }
+        
+//         return str1
+// }
+// }
+
+
+
+
+
+// if (i % 3 !== 0 && i % 5 !== 0) {
+//     str.concat(".")
+// }
+
+
+// function fizzbuzz (inputNumber) {
+
+//     var str1 = ""
+
+//     for (var i = 1; i <= inputNumber; i++) {
+//         if (i % 3 === 0) {
+//             return "fizz"
+//         }
+//         else if (i % 5 === 0) {
+//             return "buzz"
+//         }
+//         else if (i % 3 === 0 && i % 5 === 0) {
+//             return "fizzbuzz"
+//         }
+//         else return "."
+//     }
+//     return str1
+// }
+// function fizzbuzz () {
+//     for (let i = 1; i <= 30; i++) {
+//         if (i % 3 !== 0 || i % 5 !== 0) {
+//             return ('.')
+//         }
+//         else if (i % 3 === 0 && i % 5 !== 0) {
+//             return ('fizz')
+//         }
+//         else if (i % 5 === 0 && i % 3 !== 0) {
+//             return ('buzz')
+//         }
+//         else if (i % 3 === 0 && i % 5 ===0) {
+//             return ('fizzbuzz')
+//         }
+//     }
+// }
+
+
+// if (i % 3 !== 0 || i % 5 !==0) 
+// console.log('fizzbuzz' + ".")
+// else if (i % 3 === 0 && i % 5 !== 0)
+// console.log('fizzbuzz' + 'fizz')
+// else if (i % 5 === 0 && i % 3 !== 0)
+// console.log('fizzbuzz' + 'buzz')
+// else (i % 3 === 0 && i % 5 === 0)
+// console.log('fizzbuzz' + 'fizzbuzz')
+
+// function fizzbuzz () {
+//     for (i = 0, i < 30, i++) {
+//         if (i % 3 !== 0 || i % 5 !== 0) {
+//             return ('fizzbuzz' + ".")
+//         }
+//     }
+
+// }
+
+// var str1 = "."
+// for (index = 0, index < 50, i++) {
+//     if (index % 3 !== 0 || index % 5 !== 0) {
+//         return "."
+//     }
+// }
